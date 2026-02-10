b22989bdf03b0b08b902c5b0eb5561631413700a
SangWon7242 <gotkdqja@gmail.com>
2026-02-10 17:46:31 +0900

    요구사항3 완료

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int lastArticleId = 0;
     System.out.println("== 자바 게시판 시작 ==");
 
     while (true) {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
+
       if(cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 작성 ==");
         System.out.print("제목 : ");
@@ -18,7 +20,7 @@ public class Main {
         System.out.print("내용 : ");
         String content = sc.nextLine();
 
-        int id = 1;
+        int id = ++lastArticleId;
         System.out.printf("%d번 게시물이 작성되었습니다.\n", id);
       }
       else if(cmd.equals("exit")) {
