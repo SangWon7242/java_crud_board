8ec719e6407a50ebf07b048f761329675c90e700
SangWon7242 <gotkdqja@gmail.com>
2026-02-23 16:28:23 +0900

    ArticleService에서 검색 키워드 필터링 로직을 별도의 메서드로 분리

--- src/main/java/com/domain/article/article/service/ArticleService.java
+++ src/main/java/com/domain/article/article/service/ArticleService.java
@@ -16,17 +16,23 @@ public class ArticleService {
     articleRepository = Container.getArticleRepository();
   }
 
-  public List<Article> getArticles(String orderBy, String searchKeyword) {
-    // articleRepository.findAll() 메서드를 통해 가져온 원본 리스트를 바탕으로 새로운 리스트 객체 생성
-    List<Article> articles = new ArrayList<>(articleRepository.findAll());
-
-    List<Article> searchResults = articles;
+  // 검색 키워드를 기반으로 한 게시물 리스트 필터링
+  private List<Article> getSearchResults(List<Article> articles, String searchKeyword) {
     if(!searchKeyword.isEmpty()) {
-      searchResults = articles.stream()
+      articles = articles.stream()
           .filter(article -> article.getTitle().contains(searchKeyword) || article.getContent().contains(searchKeyword))
           .collect(Collectors.toList());
     }
 
+    return articles;
+  }
+
+  public List<Article> getArticles(String orderBy, String searchKeyword) {
+    // articleRepository.findAll() 메서드를 통해 가져온 원본 리스트를 바탕으로 새로운 리스트 객체 생성
+    List<Article> articles = new ArrayList<>(articleRepository.findAll());
+    List<Article> searchResults = getSearchResults(articles, searchKeyword);
+
+    // 정렬 로직
     if("idAsc".equals(orderBy)) {
       // Article 객체의 id 값을 기준으로 오름차순 정렬
       searchResults.sort(Comparator.comparingInt(Article::getId));
