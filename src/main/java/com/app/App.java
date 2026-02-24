4006c07356f3e38f867cefbe395c5cde31651ce7
SangWon7242 <gotkdqja@gmail.com>
2026-02-24 16:14:51 +0900

    로그인 기능 구현

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -39,7 +39,9 @@ public class App {
         articleController.doDelete(rq);
       } else if (rq.getActionPath().equals("/usr/member/join")) {
         memberController.doJoin(rq);
-      } else if (cmd.equals("exit")) {
+      } else if (rq.getActionPath().equals("/usr/member/login")) {
+        memberController.doLogin(rq);
+      }  else if (cmd.equals("exit")) {
         break;
       } else {
         System.out.println("명령어 확인 후 다시 입력해주세요.");
--- src/main/java/com/domain/member/controller/MemberController.java
+++ src/main/java/com/domain/member/controller/MemberController.java
@@ -85,4 +85,61 @@ public class MemberController {
     Member member = memberService.join(username, password, name);
     System.out.printf("'%s'님 회원 가입되었습니다.\n", member.getName());
   }
+
+  public void doLogin(Rq rq) {
+    String username;
+    String password;
+    Member member;
+
+    System.out.println("== 로그인 ==");
+
+    // 아이디 입력
+    while (true) {
+      System.out.print("아이디 : ");
+      username = Container.getSc().nextLine();
+
+      if(username.trim().isEmpty()) {
+        System.out.println("아이디를 입력해주세요.");
+        continue;
+      }
+
+      member = memberService.findByUsername(username);
+
+      if(member == null) {
+        System.out.println("존재하지 않는 아이디입니다. 다시 입력해주세요.");
+        continue;
+      }
+
+      break;
+    }
+
+    int passwordTryMaxCount = 3;
+    int tryMaxCount = 0;
+
+    // 비밀번호 입력
+    while (true) {
+      if(passwordTryMaxCount == tryMaxCount) {
+        System.out.println("비밀번호 입력 횟수를 초과하였습니다.");
+        return;
+      }
+
+      System.out.print("비밀번호 : ");
+      password = Container.getSc().nextLine();
+
+      if(password.trim().isEmpty()) {
+        System.out.println("비밀번호를 입력해주세요.");
+        continue;
+      }
+
+      if(!member.getPassword().equals(password)) {
+        System.out.println("비밀번호를 확인 후 다시 입력해주세요.");
+        System.out.printf("비밀번호 틀린 횟수 : %d / %d\n", ++tryMaxCount, passwordTryMaxCount);
+        continue;
+      }
+
+      break;
+    }
+
+    System.out.printf("'%s' 님 환영합니다.\n", member.getName());
+  }
 }
